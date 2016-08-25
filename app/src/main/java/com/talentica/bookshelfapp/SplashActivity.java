909ee724169b4f6fcf06dfbf516d15341a2a7b44
rohan-ratnaparkhi <rohanr@talenticaadmins-Mac-mini-3.local>
2016-08-25 14:51:29 +0530

    added splash screen

--- app/src/main/java/com/talentica/bookshelfapp/SplashActivity.java
+++ app/src/main/java/com/talentica/bookshelfapp/SplashActivity.java
@@ -1,13 +1,39 @@
 package com.talentica.bookshelfapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Context ctx;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        ctx = this;
+
+        // Hide the status bar.
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+
+        //        displayLoginPage();
+
+    }
+
+    private void displayLoginPage(){
+//        Intent intent = new Intent(ctx, LoginActivity.class);
+//        startActivity(intent);
+//        finish();
     }
 }
