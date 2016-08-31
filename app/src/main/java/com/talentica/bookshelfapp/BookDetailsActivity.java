f42c249a84143fc45c1289756c98917020427854
rohan-ratnaparkhi <rohanr@talenticaadmins-Mac-mini-3.local>
2016-08-31 11:11:14 +0530

    book borrow action with server integration

--- app/src/main/java/com/talentica/bookshelfapp/BookDetailsActivity.java
+++ app/src/main/java/com/talentica/bookshelfapp/BookDetailsActivity.java
@@ -1,13 +1,21 @@
 package com.talentica.bookshelfapp;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,6 +26,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
 
@@ -51,6 +60,7 @@ public class BookDetailsActivity extends AppCompatActivity implements View.OnCli
     TextView mTvComments;
     Button mBtnBorrow;
     Context ctx;
+    String bookId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,13 +70,47 @@ public class BookDetailsActivity extends AppCompatActivity implements View.OnCli
         setLayoutFields();
         mBtnBorrow.setOnClickListener(this);
         Bundle bundle = getIntent().getExtras();
-        String bookId = (String) bundle.get("bookId");
+        bookId = (String) bundle.get("bookId");
         populateBookDetails(bookId);
     }
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.act_book_dtls_btn_borrow:
+                final Dialog dialog = new Dialog(ctx);
+                dialog.setContentView(R.layout.custom_dialog);
+                TextView dialogTitle = (TextView) dialog.findViewById(R.id.custom_dialog_title);
+                dialogTitle.setText(getString(R.string.text_confirmation));
+                TextView dialogBodyText = (TextView) dialog.findViewById(R.id.custom_dialog_body_text);
+                dialogBodyText.setText(getString(R.string.book_borrow_confirm_text));
+                Button dialogBtnNo = (Button) dialog.findViewById(R.id.btn_borrow_no);
+                Button dialogBtnYes = (Button) dialog.findViewById(R.id.btn_borrow_yes);
+                dialogBtnNo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialogBtnYes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        confirmBookBorrow();
+                        dialog.dismiss();
+                    }
+                });
+                Window window = dialog.getWindow();
+//                window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+                WindowManager.LayoutParams wlp = window.getAttributes();
 
+                wlp.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                wlp.gravity = Gravity.BOTTOM;
+                wlp.dimAmount = (float) 0.3;
+
+                window.setAttributes(wlp);
+                dialog.show();
+                break;
+        }
     }
 
     private void setLayoutFields() {
@@ -155,4 +199,60 @@ public class BookDetailsActivity extends AppCompatActivity implements View.OnCli
         }
     }
 
+
+    private void confirmBookBorrow() {
+        try {
+//            TODO - check once the API is working
+//            Log.d("Rohan", "id:"+bookId+" ;tok: " + sharedPref.getString(Constants.USER_TOKEN, ""));
+            StringRequest req = new StringRequest(Request.Method.PUT,
+                    Constants.BASE_URL + Constants.BORROW_BOOK_API,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("Rohan", response.toString());
+                            displayResponseSnack("Your request has been successfully sent.", "success");
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d("Rohan", error.toString() + "---" + error.getLocalizedMessage());
+                            displayResponseSnack("Your request could not be proceeded. Please try later.", "error");
+                        }
+                    }) {
+
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("bookId", bookId);
+                    return params;
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<String, String>();
+                    headers.put(Constants.KEY_AUTHORIZATION, Constants.AUTH_PREPEND + StoredUser.getStoredUser(ctx).getUserToken());
+                    return headers;
+                }
+            };
+
+            SingletonRequestQueue.getInstance(ctx).addToRequestQueue(req);
+        } catch (Exception e) {
+            Log.d("Rohan", "error occurred: " + e.getMessage());
+        }
+    }
+
+    private void displayResponseSnack(String msg, String type) {
+        Snackbar snack = Snackbar.make(findViewById(R.id.book_dtls_layout), msg, Snackbar.LENGTH_LONG);
+        View view = snack.getView();
+        FrameLayout.LayoutParams params =(FrameLayout.LayoutParams)view.getLayoutParams();
+        params.gravity = Gravity.TOP;
+        if(type.equalsIgnoreCase("error")){
+            view.setBackgroundColor(Color.parseColor(getString(R.string.snack_error_bg)));
+        } else if (type.equalsIgnoreCase("success")){
+            view.setBackgroundColor(Color.parseColor(getString(R.string.snack_success_bg)));
+        }
+        view.setLayoutParams(params);
+        snack.show();
+    }
 }
